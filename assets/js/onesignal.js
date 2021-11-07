a21097ae44367d002877cb86d50195f2199ba1bb
Pasha Bezuglyy <eskapiste@gmail.com>
2021-11-07 19:42:51 +0200

    удалил скрипты OneSignal

--- assets/js/onesignal.js
+++
@@ -1,6 +0,0 @@
-          window.OneSignal = window.OneSignal || [];
-          OneSignal.push(function() {
-            OneSignal.init({
-              appId: "a9f42a44-9db9-4ac5-9502-5875c5cfde65",
-            });
-          });
